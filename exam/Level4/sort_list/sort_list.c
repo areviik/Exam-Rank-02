b07540f3fd684f3bca82655e5f53015136b4dcaa
areviik <vardanyanarev48@gmail.com>
2025-03-23 00:26:06 +0400

    Add files via upload

---
+++ exam/Level4/sort_list/sort_list.c
@@ -0,0 +1,20 @@
+#include "ft_list.h"
+
+t_list *sort_list(t_list* lst, int (*cmp)(int, int)) 
+{
+	int swap;
+	t_list* curr = lst;
+	while(lst->next) 
+    {
+		if(((*cmp)(lst->data, lst->next->data)) == 0) 
+        {
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = curr;
+		}
+        else lst = lst->next;
+	}
+	lst = curr;
+	return lst;
+}
