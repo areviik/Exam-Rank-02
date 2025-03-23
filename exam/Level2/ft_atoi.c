9da8767e24555f96183ddd0c10d35f8ae3661df5
Arevik <vardanyanarev48@gmail.com>
2025-03-23 23:21:29 +0400

    Update ft_atoi.c

--- exam/Level2/ft_atoi.c
+++ exam/Level2/ft_atoi.c
@@ -6,18 +6,14 @@ int	ft_atoi(const char *str)
 	number = 0;
 	sign = 1;
 	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
-	{
 		str++;
-	}
 	if (*str == '-')
 	{
 		sign = -1;
 		str++;
 	}
 	else if (*str == '+')
-	{
 		str++;
-	}
 	while (*str >= '0' && *str <= '9')
 	{
 		number = number * 10 + (*str - '0');
