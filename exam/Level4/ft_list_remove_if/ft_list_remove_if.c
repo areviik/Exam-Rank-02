5af527d12fcdafb109b8d1abf6109434e24e0dc0
Arevik <vardanyanarev48@gmail.com>
2025-03-25 00:58:21 +0400

    Update ft_list_remove_if.c

--- exam/Level4/ft_list_remove_if/ft_list_remove_if.c
+++ exam/Level4/ft_list_remove_if/ft_list_remove_if.c
@@ -1,34 +1,31 @@
-#include "ft_list.h"
 #include <stdlib.h>
+#include "ft_list.h"
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp) ())
 {
-    t_list *current;
-    t_list *prev;
-    t_list *to_free;
+	t_list *head;
+	t_list *past;
+	t_list *t_remove;
 
-    if (begin_list == NULL || cmp == NULL)
-        return;
-
-    prev = NULL;
-    current = *begin_list;
-    while (current != NULL)
-    {
-        if (cmp(current->data, data_ref) == 0)
-        {
-            to_free = current;
-            if (prev == NULL)
-                *begin_list = current->next;
-            else
-                prev->next = current->next;
-            current = current->next;
-            free(to_free->data);
-            free(to_free);
-        }
-        else
-        {
-            prev = current;
-            current = current->next;
-        }
-    }
+	head = *begin_list;
+	past = NULL;
+	t_remove = NULL;
+	while (head != NULL)
+	{
+		if ((*cmp)(head->data, data_ref) == 0)
+		{
+			if (head == *begin_list)
+				*begin_list = head->next;
+			else
+				past->next = head->next;
+			t_remove = head;
+			head = head->next;
+			free(t_remove);
+		}
+		else
+		{
+			past = head;
+			head = head->next;
+		}
+	}
 }
