1e7850e9228a6eae7e306bf6dc25b15cc0512999
Arevik <vardanyanarev48@gmail.com>
2025-03-25 23:35:47 +0400

    Update flood_fill.c

--- exam/Level4/flood_fill/flood_fill.c
+++ exam/Level4/flood_fill/flood_fill.c
@@ -1,6 +1,6 @@
 #include "flood_fill.h"
 
-void flood_fill_recursive(char **tab, t_point size, int x, int y, char old_char, char new_char)
+static void flood_fill_recursive(char **tab, t_point size, int x, int y, char old_char, char new_char)
 {
     if (x < 0 || x >= size.x || y < 0 || y >= size.y || tab[y][x] != old_char)
         return;
