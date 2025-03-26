9623ff41c3f0b13134ec688128ec5a0e5201e8fe
Arevik <vardanyanarev48@gmail.com>
2025-03-26 20:40:08 +0400

    Update sort_list.c

--- exam/Level4/sort_list/sort_list.c
+++ exam/Level4/sort_list/sort_list.c
@@ -5,15 +5,16 @@ t_list *sort_list(t_list* lst, int (*cmp)(int, int))
 	int swap;
 	t_list* curr = lst;
 	while(lst->next) 
-    {
-		if(((*cmp)(lst->data, lst->next->data)) == 0) 
+    	{
+	if(((*cmp)(lst->data, lst->next->data)) == 0) 
         {
-			swap = lst->data;
-			lst->data = lst->next->data;
-			lst->next->data = swap;
-			lst = curr;
-		}
-        else lst = lst->next;
+		swap = lst->data;
+		lst->data = lst->next->data;
+		lst->next->data = swap;
+		lst = curr;
+	}
+        else 
+		lst = lst->next;
 	}
 	lst = curr;
 	return lst;
