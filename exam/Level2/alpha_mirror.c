9dcd026f8714a74edb2dffe92205c9eb6e41c371
Arevik <vardanyanarev48@gmail.com>
2025-03-29 13:09:45 +0400

    Update alpha_mirror.c

--- exam/Level2/alpha_mirror.c
+++ exam/Level2/alpha_mirror.c
@@ -1,19 +1,16 @@
 #include <unistd.h>
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-    unsigned int i = 0;
+    int i = 0;
     if (argc == 2)
     {
         while(argv[1][i])
         {
-            if (argv[1][i] >= 65 && argv[1][i] <= 90)
-            {
-                argv[1][i] = 90 - argv[1][i] + 65;
-            }
-            else if (argv[1][i] >= 97 && argv[1][i] <= 122)
-            {
-                argv[1][i] = 122 - argv[1][i] + 97;
-            }
+            if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+                argv[1][i] = 'Z' - argv[1][i] + 'A';
+            else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                argv[1][i] = 'z' - argv[1][i] + 'a';
             write(1,&argv[1][i],1);
             i++;
         }
