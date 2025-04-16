03b3932b455913cca5b8fc1f5b018e18d4d03fc0
Arevik <vardanyanarev48@gmail.com>
2025-04-16 20:24:13 +0400

    Update rot_13.c

--- exam/Level1/rot_13.c
+++ exam/Level1/rot_13.c
@@ -1,27 +1,19 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    int i = 0;
     if (argc == 2)
     {
+        int i = 0;
         while (argv[1][i])
         {
-            char c = argv[1][i];
-            if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
-            {
-                c += 13;
-                write(1, &c, 1);
-            }
-            else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
-            {
-                c -= 13;
-                write(1, &c, 1);
-            }
-            else
-                write(1, &c, 1);
+            if ((argv[1][i] >= 'A' && argv[1][i] <= 'M') || (argv[1][i] >= 'a' && argv[1][i] <= 'm'))
+                argv[1][i] += 13;
+            else if  ((argv[1][i] >= 'N' && argv[1][i] <= 'Z') || (argv[1][i] >= 'n' && argv[1][i] <= 'z'))
+                argv[1][i] -= 13;
+            write(1,&argv[1][i],1);
             i++;
         }
     }
-    write(1, "\n", 1);
+    write(1,"\n",1);
 }
