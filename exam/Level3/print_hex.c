d65da4dfc14d74430bb3a751a222bdca2a7cbeda
Arevik <vardanyanarev48@gmail.com>
2025-04-16 22:12:36 +0400

    Update print_hex.c

--- exam/Level3/print_hex.c
+++ exam/Level3/print_hex.c
@@ -2,8 +2,8 @@
 
 unsigned int ft_atoi(const char *str)
 {
-    unsigned int number;
-    while (*str && *str >= '0' && *str <= '9')
+    unsigned int number = 0;
+    while (*str >= '0' && *str <= '9')
     {
         number = number * 10 + (*str - '0');
         str++;
@@ -11,22 +11,18 @@ unsigned int ft_atoi(const char *str)
     return (number);
 }
 
-void print_hex(int num)
+static void print_hex(int num)
 {
     char *lchex = "0123456789abcdef";
-    char c;
-
     if (num >= 16)
         print_hex(num / 16);
-    c = lchex[num % 16];
-    write(1, &c, 1);
+    char c = lchex[num % 16];
+    write(1, &c,1);
 }
 
 int main(int argc, char **argv)
 {
-    if(argc == 2)
-    {
+    if (argc == 2)
         print_hex(ft_atoi(argv[1]));
-    }
-    write(1,"\n",1);
+    write(1, "\n", 1);
 }
