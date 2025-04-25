ebec4da238e49d3dbc542726530294e3819777ae
areviik <vardanyanarev48@gmail.com>
2025-04-25 20:50:35 +0400

    Update ft_atoi_base.c

--- exam/Level3/ft_atoi_base.c
+++ exam/Level3/ft_atoi_base.c
@@ -1,50 +1,37 @@
-int ft_isspace(int c)
-{
-    return (c == ' ' || (c >= '\t' && c <= '\r'));
-}
-
-int isvalid(int ch, int baselen)
-{
-    char *lcbase = "0123456789abcdef";
-    char *ucbase = "0123456789ABCDEF";
-    int i = 0;
-    while (i < baselen)
-    {
-        if (ch == lcbase[i] || ch == ucbase[i])
-            return 1;
-        i++;
-    }
-    return 0;
-}
-
 int ft_atoi_base(const char *str, int str_base)
 {
-    int num = 0, sign = 1, i = 0;
+    int num = 0, sign = 1, digit;
 
-    if (str_base < 2 || str_base > 16) 
-    return 0;
+    if (str_base < 2 || str_base > 16)
+        return 0;
 
-    while (ft_isspace(str[i]))
-        i++;
-    if (str[i] == '+')
-        i++;
-    else if (str[i] == '-')
+    while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+        str++;
+
+    if (*str == '+' || *str == '-')
     {
-        sign = -1;
-        i++;
+        if (*str == '-')
+            sign = -1;
+        str++;
     }
-    while (str[i] && isvalid(str[i], str_base))
-    {
-        int val = 0;
-        if (str[i] >= '0' && str[i] <= '9')
-            val = str[i] - '0';
-        else if (str[i] >= 'a' && str[i] <= 'f')
-            val = str[i] - 'a' + 10;
-        else if (str[i] >= 'A' && str[i] <= 'F')
-            val = str[i] - 'A' + 10;
 
-        num = num * str_base + val;
-        i++;
+    while (*str)
+    {
+        if (*str >= '0' && *str <= '9')
+            digit = *str - '0';
+        else if (*str >= 'a' && *str <= 'f')
+            digit = *str - 'a' + 10;
+        else if (*str >= 'A' && *str <= 'F')
+            digit = *str - 'A' + 10;
+        else
+            break;
+
+        if (digit >= str_base)
+            break;
+
+        num = num * str_base + digit;
+        str++;
     }
-    return (num * sign);
+
+    return num * sign;
 }
